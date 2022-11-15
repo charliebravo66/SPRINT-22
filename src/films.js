54c77c4ab4c1c498818cca51c1e0f91c281d0102
Guidarom <guidodaniel.davila@gmail.com>
2022-11-15 12:35:59 +0100

    11

--- src/films.js
+++ src/films.js
@@ -17,41 +17,73 @@ return pDire;
 
 // Exercise 3: Calculate the average of the films of a given director.
 function moviesAverageOfDirector(array, director) {
-  
-let calculaPuntuacion = array.reduce(puntuaScore => puntuaScore.score == director);
-//const calculaPuntuacion = array.reduce(
-//  (previousValue, currentValue) => previousValue + currentValue (score, director)
-//);
-/*let calculaPuntuacion =score.reduce(
-  function (valorAnterior, valorActual) {
-    return Object.assign(valorAnterior, { [valorActual.id]: valorActual });
-  },
-  {} /* el valor inicial es un objeto vacío */
 
-  //return p + e;
-//let puntuaPeli = newArray.director == calculaPuntuacion;
-return calculaPuntuacion;
+//let resultado busqueda director película  
+let result = array.filter(busquedaPeli =>busquedaPeli.director == director);
+//let calculaPuntuacion = result.map(puntuaScore => puntuaScore.score);
+const sumaPuntuaciones = array.reduce((contador, item) => contador + item.score, 0);
+
+//la reducción de elementos se divide por el número de elementos del objeto 
+let promedioPuntuacion = sumaPuntuaciones / array.length;
+
+console.log(promedioPuntuacion.toFixed(2));
+
+//se aplica en el promedio los 2 decimales
+return promedioPuntuacion;
 
     
 }
 
 // Exercise 4:  Alphabetic order by title 
 function orderAlphabetically(array) {
-  
+  let titulosPeliculas = array.map(pelicula => pelicula.title).sort();
+  return titulosPeliculas.length > 20 ? titulosPeliculas.slice(0, 20) : titulosPeliculas;
 }
 
 // Exercise 5: Order by year, ascending
-function orderByYear() {
+function orderByYear(array) {
+
+  let titulosPeliculas2 = array.map(pelicula2 => pelicula2.title);
+ 
+  let anoPeliculas = array.map(anoPelicula => anoPelicula.year);
+
+   
+  //array de pelis, devuelve un array de pelis ordenadas por año
+ // return titulosPeliculas2.sort((a,b)=> {return a.title - b.year});
+
+ return titulosPeliculas2 = array.sort((r1, r2) => (r1.year > r2.year) ? 1 : (r1.year < r2.year) ? -1 : 0);
+ //ordenar pelis que tienen el mismo año, orden alfabetico del título
+ return ordernarPelis = array.sort((s1, s2) => (s1.title == s2.year) ? 1 : (s1.title == s2.year) ? -1 : 0); 
+
+  //let filtrajeAlfabetico = titulosPeliculas2.filter(alfaPeli =>alfaPeli.title == title);
+
+  //return filtrajeAlfabetico;
+
+
+
 
 }
 
 // Exercise 6: Calculate the average of the movies in a category
 function moviesAverageByCategory() {
 
+  
+  //let puntuaPeliculas = array.filter(scorePeli =>scorePeli.score == score);
+
 }
 
 // Exercise 7: Modify the duration of movies to minutes
 function hoursToMinutes() {
+
+   let horasMinutos = array.map(duracionPeli =>duracionPeli.duration);
+  // hora a convertir
+//return t = horasMinutos;
+
+// creamos una fecha genérica con tu tiempo
+//let d = new Date(tiempo + t);
+
+// calculamos los minutos a partir de las horas y minutos de la fecha creada
+//let minutos = d.getHours() * 60 + d.getMinutes();
 
 }
 
